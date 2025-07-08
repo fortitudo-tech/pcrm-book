d1d2acddb8428aea2c7cc2803b24d8b2988ba44e
Anton Vorobets <82604278+antonvorobets@users.noreply.github.com>
2025-07-08 17:22:30 +0200

    v1.0.3 (#56)
    * README update
    
    * Poetry and version update

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.0"
+__version__ = "1.0.3"
 
 __license__ = """This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License as published by
